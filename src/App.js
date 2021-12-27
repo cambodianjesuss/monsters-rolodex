7594fdb62b4c49bad1fdae978e6a58f3880fcc46
Boi Soth <cambodianjesuss@gmail.com>
2021-12-27 02:26:07 -0800

    using api call for users

--- src/App.js
+++ src/App.js
@@ -24,6 +24,12 @@ class App extends Component {
     }
   };
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+     .then(response => response.json())
+     .then(users => console.log(users))
+  }
+
   render(){
     return(
       <div className="App">

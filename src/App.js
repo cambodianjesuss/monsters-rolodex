e98b4f5628473bfdf68b5bdd08929b0b378b58a1
Boi Soth <cambodianjesuss@gmail.com>
2021-12-27 01:23:21 -0800

    interpolating state object data in jsx

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Boi</p>
+        <p>{this.state.string}</p>
         <a
           className="App-link"
           href="https://reactjs.org"

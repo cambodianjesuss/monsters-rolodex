161a0f47945e41c6b1386cbe58e8dfd65650da03
Boi Soth <cambodianjesuss@gmail.com>
2021-12-27 14:23:07 -0800

    redoing and class for repetition

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends Component{
 
-  constructor() {
+  constructor(){
     super();
     this.state = {
       monsters: []
     }
-  };
+  }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
-     .then(response => response.json())
-     .then(users => this.setState({ monsters: users}))
+      .then(res => res.json())
+      .then(users => this.setState({monsters: users}))
   }
 
   render(){

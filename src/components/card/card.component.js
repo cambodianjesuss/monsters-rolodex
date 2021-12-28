f299c06070de8855b3ab78236f78b86cf3bec4ca
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 00:59:34 -0800

    adding image for each monster card

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -4,6 +4,7 @@ import './card.styles.css'
 export const Card = (props) => {
   return(
     <div className='card-container'>
+      <img alt={props.monster.name} src={`https://robohash.org/${props.monster.id}?set-set2&size=180x180`} />
       <h1>{props.monster.name}</h1>
     </div>
   )

3eea002c5f4dc8afd4c271f018b465c92240effc
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 00:10:50 -0800

    props and prop children notes

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -9,3 +9,14 @@ export const CardList = (props) => {
 //   console.log(props)
 //   return(<div>Hello, 2</div>)
 // }
+
+/*
+Below is a prop, accessed as object property, prop.age
+<SomeComponent age="24"></SomeComponent>
+
+Below represents prop children between the component, accessed prop.children
+<SomeComponent age="24">
+  <p>This is a prop child</p>
+  <p>This is another prop child</p>
+</SomeComponent>
+*/

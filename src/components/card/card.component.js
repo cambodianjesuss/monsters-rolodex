ddbbcc2104a6cd513b1f593701c1b3575d496f8d
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 00:54:26 -0800

    adding styling and transition transform

--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -3,7 +3,7 @@ import './card.styles.css'
 
 export const Card = (props) => {
   return(
-    <div className='card'>
+    <div className='card-container'>
       <h1>{props.monster.name}</h1>
     </div>
   )

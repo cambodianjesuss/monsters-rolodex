6a6e6796c589e17a921b31c7ef5203c30b41e783
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 14:17:36 -0800

    adding comments for using the searchbox component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import { CardList } from './components/card-list/card-list.component'
 import { SearchBox } from "./components/search-box/search-box.component";
-import TestComponent from "./components/test-component/test.component";
+// import TestComponent from "./components/test-component/test.component";
 import './App.css';
 
 class App extends Component {
@@ -23,15 +23,16 @@ class App extends Component {
 
   render(){
 
+    // Using state but not modifying data, just reading change
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+    
     return(
       <div className="App">
         <SearchBox 
           placeholder='search monsters'
           handleChange={e => this.setState({ searchField: e.target.value})}
         />
-        <TestComponent />
         <CardList monsters={filteredMonsters} />
       </div>
     )
--- src/components/search-box/search-box.component.js
+++ src/components/search-box/search-box.component.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./search-box.styles.css"
 
+// Takes a placeholder value as props
+// Takes a callbackfunction in handleChange prop
+
 export const SearchBox = ({placeholder, handleChange}) => {
   return(
     <input

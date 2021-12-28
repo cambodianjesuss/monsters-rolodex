30e0c28eb9437140e3113ac75a818c74a662600f
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 02:15:09 -0800

    logging state

--- src/App.js
+++ src/App.js
@@ -24,12 +24,12 @@ class App extends Component {
       <div className="App">
         <input 
           type='search' 
-          placeholder='search monsters' 
+          placeholder='search monsters'
           onChange={e => {
             this.setState({ searchField: e.target.value});
             console.log(this.state.searchField)
-          }
-        } />
+          }}   
+        />
         <CardList monsters={this.state.monsters} />
       </div>
     )

535898bdfd058c578f0d1fb6c339da7f95133b51
Boi Soth <cambodianjesuss@gmail.com>
2021-12-28 01:28:44 -0800

    logging searchfield

--- src/App.js
+++ src/App.js
@@ -22,8 +22,14 @@ class App extends Component {
   render(){
     return(
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({ searchField: e.target.value})} />
-        {console.log(this.state.searchField)}
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={e => {
+            this.setState({ searchField: e.target.value});
+            console.log(this.state.searchField)
+          }
+        } />
         <CardList monsters={this.state.monsters} />
       </div>
     )
